54a2957d8b10aa53da7395e4fa83c541ff4cf889
azja <andrzej.biborski@gmail.com>
2013-05-28 13:58:14 +0200

    Update runall.sh
    runall.sh is up to now automated

--- test/runall.sh
+++ test/runall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
-cd bin
-
-./compare
-./hostarray
-./hostvector
-./devicearray
-./devicevector
-
+cd bin 
+selfName=`basename $0`                                    #prevent self-run and inifite loop
+for fileName in *
+ do
+  executable=$fileName
+   if [[ -x $fileName ]] && [[ $fileName != $selfName  ]]
+    then 
+    ./$executable
+   fi
+done
 cd ..
